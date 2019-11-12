12589d98d462897ba030ddbd08b5c767079c6c0b
shlokj <shlokj@gmail.com>
2019-11-12 18:21:32 +0530

    Created activity to enter otp, modified login activity

---
+++ app/src/main/java/co/in/prodigyschool/passiton/EnterOTPActivity.java
@@ -0,0 +1,19 @@
+package co.in.prodigyschool.passiton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class EnterOTPActivity extends AppCompatActivity {
+    String userPhoneNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_otp);
+        final EditText otpField = (EditText) findViewById(R.id.editTextOtp);
+        userPhoneNumber = getIntent().getStringExtra("USER_MOB");
+
+    }
+}
--- app/src/main/java/co/in/prodigyschool/passiton/SignInActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/SignInActivity.java
@@ -1,13 +1,28 @@
 package co.in.prodigyschool.passiton;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
-
+    String userPhoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        final EditText phnofield = (EditText) findViewById(R.id.phoneNumberField);
+        FloatingActionButton next = findViewById(R.id.fab);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userPhoneNumber = phnofield.getText().toString();
+                Intent takeOTP = new Intent(SignInActivity.this, EnterOTPActivity.class);
+                takeOTP.putExtra("USER_MOB",userPhoneNumber);
+                startActivity(takeOTP);
+            }
+        });
     }
 }
