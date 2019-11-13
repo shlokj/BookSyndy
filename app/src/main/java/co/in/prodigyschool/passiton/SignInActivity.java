35ceedff368965df337e7404332512ad0f3112fb
shlokj <shlokj@gmail.com>
2019-11-13 18:48:31 +0530

    Show SnackBar message if phone number less than 10 digits (login activity)

--- app/src/main/java/co/in/prodigyschool/passiton/SignInActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/SignInActivity.java
@@ -2,6 +2,7 @@ package co.in.prodigyschool.passiton;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,14 +15,28 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         final EditText phnofield = (EditText) findViewById(R.id.phoneNumberField);
-        FloatingActionButton next = findViewById(R.id.fab);
+        FloatingActionButton next = findViewById(R.id.fab1);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 userPhoneNumber = phnofield.getText().toString();
-                Intent takeOTP = new Intent(SignInActivity.this, EnterOTPActivity.class);
-                takeOTP.putExtra("USER_MOB",userPhoneNumber);
-                startActivity(takeOTP);
+                if (userPhoneNumber.length()==10) {
+                    Intent takeOTP = new Intent(SignInActivity.this, EnterOTPActivity.class);
+                    takeOTP.putExtra("USER_MOB", userPhoneNumber);
+                    startActivity(takeOTP);
+                }
+                else {
+                    View parentLayout = findViewById(android.R.id.content);
+                    Snackbar.make(parentLayout, "Please enter a valid phone number", Snackbar.LENGTH_SHORT)
+                            .setAction("OKAY", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                }
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
+                            .show();
+                }
             }
         });
     }
