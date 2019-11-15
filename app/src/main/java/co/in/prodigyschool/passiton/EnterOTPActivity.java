8bdff1ff823ef5755d9c25dd666de3545f1168ab
shlokj <shlokj@gmail.com>
2019-11-15 17:39:11 +0530

    Created new activity; modified OTP verification activty; other small changes

--- app/src/main/java/co/in/prodigyschool/passiton/EnterOTPActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/EnterOTPActivity.java
@@ -2,6 +2,8 @@ package co.in.prodigyschool.passiton;
 
 import android.content.Intent;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.EditText;
 
 public class EnterOTPActivity extends AppCompatActivity {
     String userPhoneNumber;
+    boolean otpCorrect=true;
+    boolean isRegisteredUser=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +25,44 @@ public class EnterOTPActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //temporary code just for testing
-                Intent startposact = new Intent(EnterOTPActivity.this, ParOrStudActivity.class);
-                startActivity(startposact);
+                //check if otp is correct here
+                // Jitendra - I've made your work easier here. Just check if the OTP is correct and assign that to otpCorrect
+                if (otpField.getText().toString().length()==4) {
+                    if (otpCorrect) {
+                        if (!isRegisteredUser) {
+                            Intent startposact = new Intent(EnterOTPActivity.this, ParOrStudActivity.class);
+                            startActivity(startposact);
+                        }
+                        else {
+                            Intent startmainact = new Intent(EnterOTPActivity.this, MainActivity.class);
+                            startActivity(startmainact);
+                        }
+                    }
+                    else {
+                        View parentLayout = findViewById(android.R.id.content);
+                        Snackbar.make(parentLayout, "Incorrect verification code", Snackbar.LENGTH_SHORT)
+                                .setAction("RE-ENTER", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        otpField.setText("");
+                                    }
+                                })
+                                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                                .show();
+                    }
+                }
+                else {
+                    View parentLayout = findViewById(android.R.id.content);
+                    Snackbar.make(parentLayout, "A valid verification code has 4 digits", Snackbar.LENGTH_SHORT)
+                            .setAction("OKAY", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                }
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                            .show();
+                }
             }
         });
     }
--- app/src/main/java/co/in/prodigyschool/passiton/ParOrStudActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/ParOrStudActivity.java
@@ -1,23 +1,51 @@
 package co.in.prodigyschool.passiton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 
 public class ParOrStudActivity extends AppCompatActivity {
+
+    boolean isParent = false;
+    int studOrPar;
+    RadioGroup studOrParButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_par_or_stud);
 
+        studOrParButtons = (RadioGroup) findViewById(R.id.studentOrParentRadioGroup);
+        studOrParButtons.clearCheck();
+
         FloatingActionButton next = findViewById(R.id.fab3);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                studOrPar = studOrParButtons.getCheckedRadioButtonId();
+                if (studOrPar==-1) {
+                    View parentLayout = findViewById(android.R.id.content);
+                    Snackbar.make(parentLayout, "Please select an option", Snackbar.LENGTH_SHORT)
+                            .setAction("Okay", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
 
+                                }
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                            .show();
+                }
+                else if (studOrPar==R.id.studentoption) {
+                    isParent=false;
+                }
+                else {
+                    isParent=true;
+                }
             }
         });
 
