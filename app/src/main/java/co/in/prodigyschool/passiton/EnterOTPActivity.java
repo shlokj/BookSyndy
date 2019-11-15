74556aed112d7450430cbb475796b0f00de27f9d
gajendra <gajendrasuvarna95@gmail.com>
2019-11-15 19:43:15 +0530

    otp logic added

--- app/src/main/java/co/in/prodigyschool/passiton/EnterOTPActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/EnterOTPActivity.java
@@ -1,59 +1,52 @@
 package co.in.prodigyschool.passiton;
 
 import android.content.Intent;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class EnterOTPActivity extends AppCompatActivity {
     String userPhoneNumber;
-    boolean otpCorrect=true;
+    boolean otpCorrect=false;
     boolean isRegisteredUser=false;
+    private String ctryCode = "+91";
+    private String verificationId;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_otp);
         final EditText otpField = (EditText) findViewById(R.id.editTextOtp);
         userPhoneNumber = getIntent().getStringExtra("USER_MOB");
-
+        mAuth = FirebaseAuth.getInstance();
         FloatingActionButton next = findViewById(R.id.fab2);
-
+        sendVerificationCode(userPhoneNumber);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //check if otp is correct here
-                // Jitendra - I've made your work easier here. Just check if the OTP is correct and assign that to otpCorrect
-                if (otpField.getText().toString().length()==4) {
-                    if (otpCorrect) {
-                        if (!isRegisteredUser) {
-                            Intent startposact = new Intent(EnterOTPActivity.this, ParOrStudActivity.class);
-                            startActivity(startposact);
-                        }
-                        else {
-                            Intent startmainact = new Intent(EnterOTPActivity.this, MainActivity.class);
-                            startActivity(startmainact);
-                        }
-                    }
-                    else {
-                        View parentLayout = findViewById(android.R.id.content);
-                        Snackbar.make(parentLayout, "Incorrect verification code", Snackbar.LENGTH_SHORT)
-                                .setAction("RE-ENTER", new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        otpField.setText("");
-                                    }
-                                })
-                                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
-                                .show();
-                    }
+                if (otpField.getText().toString().length()==6) {
+                    verifyCode(otpField.getText().toString().trim());
                 }
                 else {
                     View parentLayout = findViewById(android.R.id.content);
-                    Snackbar.make(parentLayout, "A valid verification code has 4 digits", Snackbar.LENGTH_SHORT)
+                    Snackbar.make(parentLayout, "A valid verification code has 6 digits", Snackbar.LENGTH_SHORT)
                             .setAction("OKAY", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
@@ -66,4 +59,78 @@ public class EnterOTPActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void verifyCode(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInwithCredential(credential);
+    }
+
+    private void signInwithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    if (!isRegisteredUser) {
+                        Intent startposact = new Intent(EnterOTPActivity.this, ParOrStudActivity.class);
+                        startposact.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(startposact);
+                    }
+                    else {
+                        Intent startmainact = new Intent(EnterOTPActivity.this, MainActivity.class);
+                        startmainact.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(startmainact);
+                    }
+
+                }
+                else {
+                    View parentLayout = findViewById(android.R.id.content);
+                    Snackbar.make(parentLayout, "A valid verification code has 6 digits", Snackbar.LENGTH_SHORT)
+                            .setAction("OKAY", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                }
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                            .show();
+                }
+            }
+        });
+
+    }
+
+    private void sendVerificationCode(String number){
+        final String phoneNumber = ctryCode + number;
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null){
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(EnterOTPActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    };
+
+
 }
