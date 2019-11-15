249286d2310389141b5a8a3fd56405b857dd4b37
shlokj <shlokj@gmail.com>
2019-11-15 16:49:01 +0530

    Create activity to get name

---
+++ app/src/main/java/co/in/prodigyschool/passiton/CustNameActivity.java
@@ -0,0 +1,14 @@
+package co.in.prodigyschool.passiton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cust_name);
+    }
+}
