4be45046b9d71e9d680a556afa9a0e450459e72a
gajendra <gajendrasuvarna95@gmail.com>
2019-11-26 01:13:58 +0530

    save user address

--- app/src/main/java/co/in/prodigyschool/passiton/HomeActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/HomeActivity.java
@@ -1,14 +1,32 @@
 package co.in.prodigyschool.passiton;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,6 +34,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -23,10 +42,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import co.in.prodigyschool.passiton.Data.HomeItem;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    /*location variables */
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 2;
+    private LocationAddressResultReceiver addressResultReceiver;
+    private Location currentLocation;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +88,26 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /* location service */
+        addressResultReceiver = new LocationAddressResultReceiver(new Handler());
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                currentLocation = locationResult.getLocations().get(0);
+                getAddress();
+            }
+        };
+        startLocationUpdates();
+
     }
 
     @Override
@@ -87,4 +140,154 @@ public class HomeActivity extends AppCompatActivity {
     public void onBackPressed() {
 
     }
+
+    /* location classes */
+
+    @SuppressWarnings("MissingPermission")
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // GPS location can be null if GPS is switched off
+                    if (location != null) {
+                        currentLocation = location;
+                        getAddress();
+                    }
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("add_location", "Error trying to get last GPS location");
+                            Toast.makeText(HomeActivity.this,
+                                    "Error trying to get last GPS location",
+                                    Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
+                    });
+
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getAddress() {
+
+        if (!Geocoder.isPresent()) {
+            Toast.makeText(HomeActivity.this,
+                    "Can't find current address, ",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, GetAddressIntentService.class);
+        intent.putExtra("add_receiver", addressResultReceiver);
+        intent.putExtra("add_location", currentLocation);
+        startService(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
+                } else {
+                    Toast.makeText(this, "Location permission not granted, " +
+                                    "restart the app if you want the feature",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        }
+    }
+
+    private class LocationAddressResultReceiver extends ResultReceiver {
+        LocationAddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            if (resultCode == 0) {
+                //Last Location can be null for various reasons
+                //for example the api is called first time
+                //so retry till location is set
+                //since intent service runs on background thread, it doesn't block main thread
+                Log.d("Address", "Location null retrying");
+                getAddress();
+                return;
+
+            }
+
+            if (resultCode == 1) {
+                Toast.makeText(HomeActivity.this,
+                        "Address not found, " ,
+                        Toast.LENGTH_SHORT).show();
+            return;
+            }
+
+            //String currentAdd = resultData.getString("address_result");
+
+            Map<String, Object> address = new HashMap<>();
+            address.put("addr1", resultData.getString("addr1"));
+            address.put("addr2", resultData.getString("addr2"));
+            address.put("locality", resultData.getString("locality"));
+            address.put("county", resultData.getString("county"));
+            address.put("state", resultData.getString("state"));
+            address.put("country", resultData.getString("country"));
+            address.put("post", resultData.getString("post"));
+            showResults(address);
+        }
+    }
+
+    private void showResults(Map<String, Object> Address) {
+        //Toast.makeText(getApplicationContext(),addr1+" "+addr2+" "+locality,Toast.LENGTH_LONG).show();
+        try {
+            String userId = FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection("address").document(userId)
+                    .set(Address)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("firestore_address", "DocumentSnapshot successfully written!");
+                            Toast.makeText(getApplicationContext(), "Address successfully saved!", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("firestore_address", "Error writing document", e);
+                            Toast.makeText(getApplicationContext(), "Error saving Address!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
 }
