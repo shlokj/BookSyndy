15d288f52f8ba738516bc52335c1f5cf03b2c487
shlokj <shlokj@gmail.com>
2019-11-26 17:56:39 +0530

    created empty activity

---
+++ app/src/main/java/co/in/prodigyschool/passiton/GetBookSellerLocationActivity.java
@@ -0,0 +1,14 @@
+package co.in.prodigyschool.passiton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetBookSellerLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_book_seller_location);
+    }
+}
