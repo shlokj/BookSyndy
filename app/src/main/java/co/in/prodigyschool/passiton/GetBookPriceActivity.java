3173dbea3016ebce804f7bcb2c636251f9d70699
shlokj <shlokj@gmail.com>
2019-11-26 23:37:11 +0530

    added to get price activity

--- app/src/main/java/co/in/prodigyschool/passiton/GetBookPriceActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/GetBookPriceActivity.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class GetBookPriceActivity extends AppCompatActivity {
 
     boolean isTextbook;
@@ -17,6 +20,7 @@ public class GetBookPriceActivity extends AppCompatActivity {
     Switch forFree;
     TextView pricingInstructions;
     EditText priceField;
+    int price=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,8 @@ public class GetBookPriceActivity extends AppCompatActivity {
             pricingInstructions.setText("Keep the price nominal and as low as possible, keeping in mind that you no longer need this material.\n\nFor example, if the MRP is Rs. 200, give it at less than Rs. 30.");
         }
 
+        FloatingActionButton next = (FloatingActionButton) findViewById(R.id.fab17);
+
         forFree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -55,6 +61,29 @@ public class GetBookPriceActivity extends AppCompatActivity {
                     priceField.setFocusable(false);
                     priceField.setText("0");
                     pricingInstructions.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (priceField.getText().toString().length()>0) {
+                    try {
+                        price = Integer.parseInt(priceField.getText().toString());
+                        int x = price / 1;
+                    } catch (Exception e) {
+                        View parentLayout = findViewById(android.R.id.content);
+                        Snackbar.make(parentLayout, "Please enter a valid price", Snackbar.LENGTH_SHORT)
+                                .setAction("OKAY", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+
+                                    }
+                                })
+                                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                                .show();
+                    }
                 }
             }
         });
