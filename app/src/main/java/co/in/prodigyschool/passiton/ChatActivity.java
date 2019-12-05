0c9b1b3ba8ae74175b9ec2441d4ade59f40d4924
gajendra <gajendrasuvarna95@gmail.com>
2019-12-05 21:41:29 +0530

    chat_feature

--- app/src/main/java/co/in/prodigyschool/passiton/ChatActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/ChatActivity.java
@@ -4,34 +4,20 @@ in.prodigyschool.passiton;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import android.view.View;
+
+import co.in.prodigyschool.passiton.ui.chatUtil.SectionsPagerAdapter;
 
 public class ChatActivity extends AppCompatActivity {
-
-    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
-    private static final int RC_PHOTO_PICKER = 2;
-
-
-    private CircleImageView visiter_profile_picture;
-    private String visit_user_id,visit_user_name,visit_image;
-    private TextView visiter_name;
-    private ImageButton mPhotoPickerButton;
-    private EditText mMessageEditText;
-    private Button mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,64 +25,25 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         Toolbar toolbar = findViewById(R.id.chat_toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_back_arrow_white);
-        toolbar.setTitle("");
+        toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        visiter_profile_picture = findViewById(R.id.visit_profile_image);
-        mPhotoPickerButton =  findViewById(R.id.photoPickerButton);
-        mMessageEditText = findViewById(R.id.messageEditText);
-        visiter_name = findViewById(R.id.visit_profile_name);
-        mSendButton = findViewById(R.id.sendButton);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
 
-
-
-        initializeChatRoom();
-
-    }
-
-    private void initializeChatRoom() {
-        /* populate visitors details */
-        visit_image = getIntent().getStringExtra("visit_image");
-        visit_user_id = getIntent().getStringExtra("visit_user_id");
-        visit_user_name = getIntent().getStringExtra("visit_user_name");
-        Glide.with(getApplicationContext()).load(visit_image).into(visiter_profile_picture);
-        visiter_name.setText(visit_user_name);
-
-        /* buttons and listeners */
-        // ImagePickerButton shows an image picker to upload a image for a message
-        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/jpeg");
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
-        // Enable Send button when there's text to send
-        mMessageEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.toString().trim().length() > 0) {
-                    mSendButton.setEnabled(true);
-                } else {
-                    mSendButton.setEnabled(false);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        });
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
     }
-
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -105,10 +52,7 @@ public class ChatActivity extends AppCompatActivity {
     }
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+        Intent homeActivity = new Intent(ChatActivity.this,HomeActivity.class);
+        startActivity(homeActivity);
     }
-
-
-
-
 }
