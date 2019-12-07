d1a33c576874ade75c10bb4101f89a9a2d3bf8b9
gajendra <gajendrasuvarna95@gmail.com>
2019-12-08 03:14:52 +0530

    chat feature completed

--- app/src/main/java/co/in/prodigyschool/passiton/BookDetailsActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/BookDetailsActivity.java
@@ -3,6 +3,10 @@ package co.in.prodigyschool.passiton;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +19,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -23,12 +30,16 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import co.in.prodigyschool.passiton.Data.Book;
+import co.in.prodigyschool.passiton.Data.Chat;
+import co.in.prodigyschool.passiton.Data.User;
 
 public class BookDetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String bookid;
     FirebaseFirestore mFirestore;
-    Query mQuery;
+    Book currentBook;
+    User bookOwner;
+    private FloatingActionButton fab_favourite;
     private TextView view_bookname,view_address,view_price,view_category,view_description;
     private ImageView view_bookimage;
 
@@ -54,13 +65,17 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
 
     private void initFireStore() {
         mFirestore = FirebaseFirestore.getInstance();
+
         if(mFirestore != null){
             populateBookDetails();
+
         }
         else{
             Toast.makeText(getApplicationContext(),"Firebase error",Toast.LENGTH_SHORT).show();
         }
     }
+
+
 
     private void populateBookDetails() {
         //
@@ -69,21 +84,23 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
             bookReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                 @Override
                 public void onSuccess(DocumentSnapshot snapshot) {
-                    Book book = snapshot.toObject(Book.class);
-
-                    view_bookname.setText(book.getBookName());
-                    view_description.setText(book.getBookDescription());
-                    view_address.setText(book.getBookAddress());
-                    view_price.setText("₹"+book.getBookPrice());
-                    if(book.isTextbook()){
+                    currentBook = snapshot.toObject(Book.class);
+                    view_bookname.setText(currentBook.getBookName());
+                    view_description.setText(currentBook.getBookDescription());
+                    view_address.setText(currentBook.getBookAddress());
+                    view_price.setText("₹"+currentBook.getBookPrice());
+                    if(currentBook.isTextbook()){
                         view_category.setText("Text Book");
                     }
                     else{
                         view_category.setText("Notes");
                     }
                     Glide.with(view_bookimage.getContext())
-                            .load(book.getBookPhoto())
+                            .load(currentBook.getBookPhoto())
                             .into(view_bookimage);
+
+                    populateUserDetails();
+
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -98,6 +115,34 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
+    /*
+    method to get user details;
+     */
+    private void populateUserDetails() {
+        if (!checkConnection(getApplicationContext())) {
+            Toast.makeText(getApplicationContext(),"Internet Required",Toast.LENGTH_LONG).show();
+            return;
+        }
+        DocumentReference reference = mFirestore.collection("users").document(currentBook.getUserId());
+        reference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot snapshot) {
+           if(snapshot.exists()){
+               bookOwner = snapshot.toObject(User.class);
+           }
+           else{
+               Log.d(TAG, "onSuccess: no user found for the book");
+           }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "onFailure: populateuserdetails" ,e );
+            }
+        });
+    }
+
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -105,13 +150,41 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
                 onBackArrowClicked(v);
                 break;
             case R.id.fab_chat:
+                startChatActivity();
                 break;
             case R.id.fab_favorite:
                 //break;
         }
     }
 
+    private void startChatActivity() {
+        Intent chatIntent = new Intent(BookDetailsActivity.this, ChatActivity.class);
+        chatIntent.putExtra("visit_user_id", currentBook.getUserId()); // phone number
+        chatIntent.putExtra("visit_user_name", bookOwner.getUserId()); // unique user id
+        chatIntent.putExtra("visit_image", bookOwner.getImageUrl());
+        startActivity(chatIntent);
+    }
+
+
     public void onBackArrowClicked(View view) {
         onBackPressed();
     }
+
+    public static boolean checkConnection(Context context) {
+        final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connMgr != null) {
+            NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
+
+            if (activeNetworkInfo != null) { // connected to the internet
+                // connected to the mobile provider's data plan
+                if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                    // connected to wifi
+                    return true;
+                } else return activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+            }
+        }
+        return false;
+    }
+
 }
