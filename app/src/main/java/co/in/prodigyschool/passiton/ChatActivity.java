6e1fd2904d4edeb061f48324e147db42361dfba7
gajendra <gajendrasuvarna95@gmail.com>
2019-12-07 22:41:59 +0530

    merge master to chat feature branch

--- app/src/main/java/co/in/prodigyschool/passiton/ChatActivity.java
+++ app/src/main/java/co/in/prodigyschool/passiton/ChatActivity.java
@@ -2,7 +2,15 @@ package co.
 
 in.prodigyschool.passiton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -13,10 +21,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    private static final int RC_PHOTO_PICKER = 2;
+
 
     private CircleImageView visiter_profile_picture;
     private String visit_user_id,visit_user_name,visit_image;
     private TextView visiter_name;
+    private ImageButton mPhotoPickerButton;
+    private EditText mMessageEditText;
+    private Button mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +43,57 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         visiter_profile_picture = findViewById(R.id.visit_profile_image);
+        mPhotoPickerButton =  findViewById(R.id.photoPickerButton);
+        mMessageEditText = findViewById(R.id.messageEditText);
         visiter_name = findViewById(R.id.visit_profile_name);
+        mSendButton = findViewById(R.id.sendButton);
+
+
 
         initializeChatRoom();
 
     }
 
     private void initializeChatRoom() {
-
+        /* populate visitors details */
         visit_image = getIntent().getStringExtra("visit_image");
         visit_user_id = getIntent().getStringExtra("visit_user_id");
         visit_user_name = getIntent().getStringExtra("visit_user_name");
         Glide.with(getApplicationContext()).load(visit_image).into(visiter_profile_picture);
         visiter_name.setText(visit_user_name);
 
+        /* buttons and listeners */
+        // ImagePickerButton shows an image picker to upload a image for a message
+        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+            }
+        });
+
+        // Enable Send button when there's text to send
+        mMessageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
     }
 
 
@@ -55,4 +106,6 @@ public class ChatActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+
 }
