ffec0d1f651bc936db8214acd5f362c13cfbf347
Shlok Jhawar <shlokj@gmail.com>
2020-02-23 15:22:34 +0530

    created activity to log in

---
+++ app/src/main/java/com/booksyndy/academics/android/AuthActivity.java
@@ -0,0 +1,15 @@
+package com.booksyndy.academics.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        getSupportActionBar().setTitle("Sign in");
+    }
+}
